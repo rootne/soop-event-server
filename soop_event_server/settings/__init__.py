6130105600626618436880027435c5ed64b77724
rootne <rootne@rootne.me>
2024-09-11 23:58:36 +0900

    Django settings 파일 분할

--- soop_event_server/settings/__init__.py
+++ soop_event_server/settings/__init__.py
@@ -1,1 +1,16 @@
 from .base import *
+
+SECRET_KEY = "django-insecure-$dl^qbfkt+=!#co%_app2!95(@!qnudp*4e*7)g@gbnt@1xxd="
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR_PATH / "db.sqlite3",
+    }
+}
+
+STATIC_URL = "static/"
