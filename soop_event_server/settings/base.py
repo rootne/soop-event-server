68e41f1223391a9195893b812bef75c42673ed95
rootne <rootne@rootne.me>
2024-09-15 05:49:11 +0900

    Django 앱 추가: event_manager

--- soop_event_server/settings/base.py
+++ soop_event_server/settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Project apps
     "authorization",
+    "event_manager",
 ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
